f31015441993b747fe1b1860ec3bd8d9e7a60eef
Zaratustra <lucasmauriciocarranza@gmail.com>
2024-10-30 18:11:39 -0300

    Realizamos un contador de palabras en base a una práctica de algoritmos en el cual usamos pseudocódigo y diagrama de fluojo

---
+++ contador_palabras.py
@@ -0,0 +1,11 @@
+# Paso 1: Solicitar entrada al usuario
+
+palabra_ingresada=input("Por favor, ingreses una palabla: ")
+
+# Paso 2: Contar la cantidad de letras
+
+cantidad_letras=len(palabra_ingresada)
+
+# Paso 3: Mostrar al usuario el resultado
+
+print("La palabla ingresada tiene", cantidad_letras, "letras")
